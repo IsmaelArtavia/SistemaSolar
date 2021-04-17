4d672e4a2b979b140f94bec6383ce19ec92f0e8c
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-04-17 00:25:12 +0000

    sistema solar

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,23 +2,30 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.scss";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="Sol"></div>
+
+			<div className="mercurio"></div>
+			<div className="orbitaMercurio"></div>
+			<div className="orbitaVenus"></div>
+			<div className="venus"></div>
+			<div className="orbitaTierra"></div>
+			<div className="tierra"></div>
+			<div className="orbitaMarte"></div>
+			<div className="marte"></div>
+			<div className="orbitaJupiter"></div>
+			<div className="jupiter"></div>
+			<div className="orbitaSaturno"></div>
+			<div className="saturno"></div>
+			<div className="orbitaUrano"></div>
+			<div className="urano"></div>
+			<div className="orbitaNeptuno"></div>
+			<div className="neptuno"></div>
 		</div>
 	);
 }
