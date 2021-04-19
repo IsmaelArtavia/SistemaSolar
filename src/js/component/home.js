bf815efd3d1f17045c3db4a8a792c720c54af831
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-04-19 01:50:42 +0000

    actualizacion

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -14,16 +14,44 @@ export function Home() {
 			<div className="orbitaMercurio"></div>
 			<div className="orbitaVenus"></div>
 			<div className="venus"></div>
+
 			<div className="orbitaTierra"></div>
-			<div className="tierra"></div>
+			<div>
+				<div className="orbitaLuna">
+					<div className="tierra">
+						{" "}
+						<div className="luna"></div>
+					</div>
+				</div>
+			</div>
+
 			<div className="orbitaMarte"></div>
-			<div className="marte"></div>
+			<div className="marte">
+				<div className="fobos"></div>
+				<div className="deimos"></div>
+			</div>
 			<div className="orbitaJupiter"></div>
-			<div className="jupiter"></div>
+			<div className="jupiter">
+				<div className="io"></div>
+				<div className="europa"></div>
+				<div className="ganimedes"></div>
+				<div className="calisto"></div>
+			</div>
 			<div className="orbitaSaturno"></div>
-			<div className="saturno"></div>
+			<div className="saturno">
+				<div className="titan"></div>
+				<div className="japeto"></div>
+				<div className="febe"></div>
+				<div className="rea"></div>
+			</div>
 			<div className="orbitaUrano"></div>
-			<div className="urano"></div>
+			<div className="urano">
+				<div className="titania"></div>
+				<div className="oberon"></div>
+				<div className="umbirel"></div>
+				<div className="ariel"></div>
+				<div className="miranda"></div>
+			</div>
 			<div className="orbitaNeptuno"></div>
 			<div className="neptuno"></div>
 		</div>
